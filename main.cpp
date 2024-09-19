ba6304f192b21a3278b9284611be21c1e38da7e9
ChuDinhKlopp <chutuanduc0505@gmail.com>
2024-09-19 15:56:55 +0700

    Add code

---
+++ main.cpp
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include <arm_sve.h>
+#include <benchmark/benchmark.h>
+#include <math.h>
+#include <omp.h>
+#include <chrono>
+
+template<typename T>
+void zeroMat(T *mat, int rows, int cols) {
+	for (int i = 0; i < rows * cols; i++) {
+		mat[i] = 0;
+	}
+}
+
+template<typename T>
+void printMat(T *mat, int rows, int cols) {
+	printf("===========\n");
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			std::cout << mat[i * cols + j] << ", ";
+		}
+		printf("\n");
+	}
+	printf("===========\n");
+}
+
+template<class T>
+void initMat(T *mat, int rows, int cols) {
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			mat[i * cols + j] = rand() % 10;
+		}
+	}
+}
+
+template<typename T>
+void transposeMat(T **mat, int rows, int cols) {
+	T *tmp = (T *)malloc(cols * rows * sizeof(T));
+	for (int i = 0; i < cols; i++) {
+		for (int j = 0; j < rows; j++) {
+			tmp[i * rows + j] = (*mat)[j * cols + i];
+		}
+	}
+	free(*mat);
+	*mat = tmp;
+}
+
+void matMul(T *matA, T *matB, T *matC, int M, int N, int K) {
+	int i, j, k;
+
+#pragma omp parallel for collapse(2)
+	for (i = 0; i < M; i += 4) {
+		for (j = 0; j < M; j += 4) {
+			svfloat32_t c00 = svdup_s32(0);
+			svfloat32_t c01 = svdup_s32(0);
+			svfloat32_t c02 = svdup_s32(0);
+			svfloat32_t c03 = svdup_s32(0);
+			svfloat32_t c10 = svdup_s32(0);
+			svfloat32_t c11 = svdup_s32(0);
+			svfloat32_t c12 = svdup_s32(0);
+			svfloat32_t c13 = svdup_s32(0);
+			svfloat32_t c20 = svdup_s32(0);
+			svfloat32_t c21 = svdup_s32(0);
+			svfloat32_t c22 = svdup_s32(0);
+			svfloat32_t c23 = svdup_s32(0);
+			svfloat32_t c30 = svdup_s32(0);
+			svfloat32_t c31 = svdup_s32(0);
+			svfloat32_t c32 = svdup_s32(0);
+			svfloat32_t c33 = svdup_s32(0);
+			for (k = 0; k < K; k += 8) {
+				svfloat32_t b0 = svld1_f32(svptrue_b32(), matB + (j + 0) * K + k);
+				svfloat32_t b1 = svld1_f32(svptrue_b32(), matB + (j + 0) * K + k);
+				svfloat32_t b2 = svld1_f32(svptrue_b32(), matB + (j + 0) * K + k);
+				svfloat32_t b3 = svld1_f32(svptrue_b32(), matB + (j + 0) * K + k);
+
+				svfloat32_t a0 = svld1_f32(svptrue_b32(), matA + (i + 0) * K + k));
+				c00 = svmad_f32(svptrue_b32(), a0, b0, c00);
+				c01 = svmad_f32(svptrue_b32(), a0, b1, c01);
+				c02 = svmad_f32(svptrue_b32(), a0, b2, c02);
+				c03 = svmad_f32(svptrue_b32(), a0, b3, c03);
+
+				svfloat32_t a0 = svld1_f32(svptrue_b32(), matA + (i + 1) * K + k));
+				c10 = svmad_f32(svptrue_b32(), a1, b0, c10);
+				c11 = svmad_f32(svptrue_b32(), a1, b1, c11);
+				c12 = svmad_f32(svptrue_b32(), a1, b2, c12);
+				c13 = svmad_f32(svptrue_b32(), a1, b3, c13);
+
+				svfloat32_t a0 = svld1_f32(svptrue_b32(), matA + (i + 2) * K + k));
+				c20 = svmad_f32(svptrue_b32(), a2, b0, c20);
+				c21 = svmad_f32(svptrue_b32(), a2, b1, c21);
+				c22 = svmad_f32(svptrue_b32(), a2, b2, c22);
+				c23 = svmad_f32(svptrue_b32(), a2, b3, c23);
+
+				svfloat32_t a0 = svld1_f32(svptrue_b32(), matA + (i + 3) * K + k));
+				c20 = svmad_f32(svptrue_b32(), a3, b0, c30);
+				c21 = svmad_f32(svptrue_b32(), a3, b1, c31);
+				c22 = svmad_f32(svptrue_b32(), a3, b2, c32);
+				c23 = svmad_f32(svptrue_b32(), a3, b3, c33);
+			}
+
+		}
+	}
+}
+
+int main() {
+	int M = 1024, N = 1024, K = 1024;
+	float *A, *B, *C;
+	A = (float *)malloc(M * K * sizeof(float));
+	B = (float *)malloc(K * N * sizeof(float));
+	C = (float *)malloc(M * N * sizeof(float));
+	
+	initMat<float>(A, M, K);
+	//printMat<float>(A, M, K);
+	initMat<float>(B, K, N);
+	//printMat<float>(B, K, N);
+	transposeMat<float>(&B, K, N);
+	//printMat<float>(B, K, N);
+	auto start = std::chrono::high_resolution_clock::now();
+	matMul<float>(A, B, C, M, N, K);
+	auto end = std::chrono::high_resolution_clock::now();
+	//printMat<float>(C, M, N);
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	std::cout << "Time taken: " << duration.count() << " milliseconds" << std::endl;
+	return 0;
+}
